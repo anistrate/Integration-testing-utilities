2ef38af0fbce3f355a22a310e298e11c237642c2
anistrate <istrandrei@gmail.com>
2023-06-01 18:30:12 +0300

    feat: added delete functionality

--- folder_creation_script.py
+++ folder_creation_script.py
@@ -1,12 +1,12 @@
 import os
 import json
-
+import shutil
 
 def create_recursively(depth:int, path:str) -> None:
     if depth == 0:
         return
    
-    for indexSubfolder in range(1, numberOfFolders+1):
+    for indexSubfolder in range(1, numberOfFolders):
         currentPath:str = GetPath(path, depth, indexSubfolder)
         os.mkdir(currentPath)
         create_recursively(depth-1, currentPath)
@@ -18,42 +18,57 @@ def GetPath(path:str, folderDepth:int, index:int) -> str:
 
 
 def create_folders() -> None:
-    with open('config.json', 'r') as configFile:
-        jsonConfig:dict = json.load(configFile)
-
-
-    path:str = jsonConfig['path']
-    numberOfFolders:int = jsonConfig['nrOfFolders']
-    folderDepth: int = jsonConfig['depthOfFolders']
-
-    directory_name:str = "test"
-    path += "/"+directory_name
     os.mkdir(path)
 
-    for index in range(1,numberOfFolders+1):
+    for index in range(1,numberOfFolders):
         folderPath:str = GetPath(path, folderDepth, index)
         os.mkdir(folderPath)
         create_recursively(folderDepth-1, folderPath)
     return
 
-
-
+def delete_folders() -> None:
+    deleteConfirmInvalid:bool = True
+    while deleteConfirmInvalid:
+        print("Are you sure you want to delete the folders at " + path +"?")
+        deleteConfirm:str = input("Confirm: Y/N")
+        if deleteConfirm == "Y":
+            try:
+                shutil.rmtree(path)
+                print("Folders deleted!")
+            except OSError as e:
+                print("Error : %s : %s " % (path, e.strerror))
+            deleteConfirmInvalid = False
+        elif deleteConfirm == "N":
+            deleteConfirmInvalid = False
+        else:
+            print("Invalid command!")
+    return
 
 print("Start...")
-print("Select:")
-print("1. Create folders")
-print("2. Delete Folders")
 
+directory_name:str = "test"
+with open('config.json', 'r') as configFile:
+    jsonConfig:dict = json.load(configFile)
 
-command:str = input("Command: ")
+path:str = jsonConfig['path']
+numberOfFolders:int = int(jsonConfig['nrOfFolders'])
+numberOfFolders += 1
+folderDepth:int = int(jsonConfig['depthOfFolders'])
+path += "/"+directory_name
 
-
-if command == "1" :
-    create_folders()
-#else if command == "2" :
-#    delete_folders()
-#else:
- 
-
+commandIsInvalid:bool = True
+while commandIsInvalid:
+    print("Select:")
+    print("1. Create folders at " + path)
+    print("2. Delete Folders at " + path)
+    command:str = input("Command: ")
+    if command == "1" :
+        create_folders()
+        commandIsInvalid = False
+    elif command == "2" :
+        delete_folders()
+        commandIsInvalid = False
+    else:
+        print("Invalid command.")
 
 print("Done. Execution Succesful!")
